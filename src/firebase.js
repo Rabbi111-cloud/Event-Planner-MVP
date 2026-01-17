93ccc1894023978b6f6843bb32e0f0e74cbc949f
Rabbi111-cloud <diggingdeep0002@gmail.com>
2026-01-17 08:22:09 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDcmRWvBQJ8V9cESIY6CYAK8GU3SPglWrs",
+  authDomain: "event-planner-app-af22f.firebaseapp.com",
+  projectId: "event-planner-app-af22f",
+  storageBucket: "event-planner-app-af22f.firebasestorage.app",
+  messagingSenderId: "975805639738",
+  appId: "1:975805639738:web:dd4cf508cb8e9c28649903"
 };
 
 const app = initializeApp(firebaseConfig);
